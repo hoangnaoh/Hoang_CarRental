1dc3a753d56a614826651f494925b2f2e9332917
hoangnoah <truongminhkhoikt4@gmail.com>
2026-03-17 14:40:26 +0700

    a

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'https://hoangcarrental-production.up.railway.app/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
